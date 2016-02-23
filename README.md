dbe8a57d84b3eb929241a31484365ec01357ed3e
David ML Brown Jr <dmlb2000@uber-gamer.dmlb2000.org>
2016-02-22 21:51:31 -0800

    initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# dmlb2000_chefdk
+
+TODO: Enter the cookbook description here.
+
---
+++ metadata.rb
@@ -0,0 +1,7 @@
+name 'dmlb2000_chefdk'
+maintainer 'The Authors'
+maintainer_email 'you@example.com'
+license 'all_rights'
+description 'Installs/Configures dmlb2000_chefdk'
+long_description 'Installs/Configures dmlb2000_chefdk'
+version '0.1.0'
